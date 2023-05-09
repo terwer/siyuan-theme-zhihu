0006ccff57f36db22a8956c5f00d22bd91983118
terwer <youweics@163.com>
2023-05-09 23:41:30 +0800

    feat: 初始化Zhihu主题

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from "vitest"
-import Theme from "~/src/index"
-
-describe("test theme", () => {
-  it("test main", async () => {
-    const theme = new Theme()
-    await theme.init("node")
-  })
-})
